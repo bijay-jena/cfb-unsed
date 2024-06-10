b8919d8931e20eafcaf8720026655dffe772ce56
Bijay Jena <bijay.jena@squbix.com>
2024-06-10 12:55:47 +0530

    Create switch.jsx

---
+++ switch.jsx
@@ -0,0 +1,13 @@
+<div className="d-flex align-items-top justify-content-end gap-2 flex-row">
+  <div className="secondary-text">
+    Half-Width
+  </div>
+  <div className="switch">
+    <input
+      id="isCrossTab"
+      type="checkbox"
+      // checked={flags.isCrossTab}
+      onChange={(e) => {}}
+    />
+  </div>
+</div>
